7d6b88dd82128616f276e7e9106c75ffe7fe0d06
Sahil Ansari <138162151+S4H1L9@users.noreply.github.com>
2025-02-23 18:53:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Sahil',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Evil 3:)",
+  botOwner: 'Sahil Xwd', // Your name
+  ownerNumber: '919354720853, //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
